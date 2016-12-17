bece492b452db742828292a067a1d6c63aaf698c
Javier <abrew_javier@hotmail.com>
2016-12-16 23:27:37 -0600

    Operadores aritméticos

---
+++ 003_operadores_aritmeticos/operadoresaritmeticos.py
@@ -0,0 +1,23 @@
+variableA= 1991
+variableB= 20.12
+
+# Suma
+suma= variableA + variableB
+
+# Resta
+resta= variableB - variableA
+
+# Multiplicación
+multiplicacion= variableA * variableB
+
+# Exponente. base ** potencia
+exponente= variableA ** 2
+
+# Divición entera
+divicionEntera= variableB / variableA
+
+# Divición real
+divicionReal= float(variableB) / variableA
+
+# Módulo
+modulo= variableB % 20
