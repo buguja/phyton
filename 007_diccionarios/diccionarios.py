652cf0c1af6b1fc5c9a27453f416b6ca64e6bcfa
Javier <abrew_javier@hotmail.com>
2016-12-17 00:15:26 -0600

    diccionarios

---
+++ 007_diccionarios/diccionarios.py
@@ -0,0 +1,10 @@
+diccionario= {
+    "clave1": "valor",
+    "clave2": 1991
+    12: [20, 12]
+}
+
+#para leer un elemento de un diccionario, sólo se puede hacer
+#a traves de su clave.
+
+#las claves no pueden ser cambiadas pero los valores sí.
